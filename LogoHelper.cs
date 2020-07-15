339c217ef4c832a556927b62f7a9d375afec09ec
Il Harper <afanyiyu@hotmail.com>
2020-07-15 12:08:53 +0800

    feat: Add LogoHelper Class

---
+++ LogoHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Common.Helpers
+{
+    public static class LogoHelper
+    {
+        public static string GetLogoPath(string productId) =>
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{productId}.png");
+    }
+}
