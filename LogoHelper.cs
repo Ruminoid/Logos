f0dcba83eff25288e6e54df3d2f5b5c8464b9cae
Il Harper <afanyiyu@hotmail.com>
2020-07-16 22:38:20 +0800

    feat: LogoHelper Update

--- LogoHelper.cs
+++ LogoHelper.cs
@@ -10,6 +10,8 @@ namespace Ruminoid.Common.Helpers
     public static class LogoHelper
     {
         public static string GetLogoPath(string productId) =>
-            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{productId}.png");
+            File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{productId}.png"))
+                ? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{productId}.png")
+                : Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "HeavyWork.png");
     }
 }
